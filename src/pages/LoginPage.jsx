abf561c4450851b5aaa9ed9de5c493a38c033075
giuprofilo <giulliavoloton@gmail.com>
2024-12-30 18:20:24 -0300

    add style login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -40,39 +40,101 @@ function LoginPage() {
   }
 
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="flex min-h-full justify-center items-center bg-gray-100">
+      <div className=" sm:w-full sm:max-w-sm bg-white p-8 rounded-lg shadow">
+        <h2 className="mt-6 text-center text-2xl font-bold leading-9 text-gray-900">
+          Entre na sua conta
+        </h2>
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email</label>
+        <form className="mt-10 space-y-6" onSubmit={handleSubmit}>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                value={form.email}
+                onChange={handleChange}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
 
-          <input
-            name="email"
-            type="email"
-            autoComplete="email"
-            value={form.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Senha
+            </label>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                value={form.password}
+                onChange={handleChange}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
 
-        <div>
-          <label>Senha</label>
-
-          <input
-            name="password"
-            type="password"
-            value={form.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-
-        <button>Entrar</button>
-      </form>
+          <div>
+            <button
+              type="submit"
+              className="w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Entrar
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
+  // return (
+  //   <div>
+  //     <h1>Login Page</h1>
+
+  //     <form onSubmit={handleSubmit}>
+  //       <div>
+  //         <label>Email</label>
+
+  //         <input
+  //           name="email"
+  //           type="email"
+  //           autoComplete="email"
+  //           value={form.email}
+  //           onChange={handleChange}
+  //           required
+  //         />
+  //       </div>
+
+  //       <div>
+  //         <label>Senha</label>
+
+  //         <input
+  //           name="password"
+  //           type="password"
+  //           value={form.password}
+  //           onChange={handleChange}
+  //           required
+  //         />
+  //       </div>
+
+  //       <button>Entrar</button>
+  //     </form>
+  //   </div>
+  // );
 }
 
 export default LoginPage;
